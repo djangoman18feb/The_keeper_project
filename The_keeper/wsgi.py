56c8f4213f850375a4d82e3d2c873e50ba3f96e5
djangoman18feb <djangoman18feb@gmail.com>
2017-11-19 12:18:00 -0800

    intial commit

---
+++ The_keeper/wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+path = '/home/djangoboy18dec/The_keeper'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "The_keeper.settings"
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+application = StaticFilesHandler(get_wsgi_application())
