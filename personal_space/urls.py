83de558dc3965567919d84892c4b6df089bdb05f
djangoman18feb <djangoman18feb@gmail.com>
2017-11-22 12:54:19 -0800

    second development comit until model forms and push using git in pycharm

--- personal_space/urls.py
+++ personal_space/urls.py
@@ -6,12 +6,14 @@ app_name ='personal_space'
 
 urlpatterns = [
     #/personal_space/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.Indexview.as_view(), name='index'),
     #/personal_space/topic__id
-    url(r'^(?P<topic_id>[0-9]+)/$', views.detail, name='detail'),
-
-
-
-
+    url(r'^(?P<pk>[0-9]+)/$', views.Detailview.as_view(), name='detail'),
+    #/personal_space/topic_id/delete_topic/
+    url(r'^(?P<pk>[0-9]+)/delete_topic/$', views.Delete_topic.as_view(), name='delete_topic'),
+    #/personal_space/topic/add
+    url(r'^topic/add/$', views.TopicCreate.as_view(), name='create_topic'),
+    #/personal_space/topic_id/update
+    url(r'^(?P<pk>[0-9]+)/update/$', views.TopicUpdate.as_view(), name='update_topic'),
 
 ]
